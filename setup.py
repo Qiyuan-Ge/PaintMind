d829e64b8ca06dd459cfe7d932b4d1d019e2d64a
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-02-25 15:49:43 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'paintmind',
+    version = 0.0,
+    author = 'Qiyuan Ge',
+    author_email = '542801615@qq.com',
+    keywords = [
+        'artificial intelligence',
+        'deep learning',
+        'generate model',
+    ],      
+    packages = find_packages(),
+    install_requires=[
+        'einops',
+        'numpy',
+        'pillow',
+        'torch>=1.6',
+        'torchvision',
+        'transformers',
+        'accelerate',
+        'tqdm',
+        'pycocotools',
+    ],
+)
