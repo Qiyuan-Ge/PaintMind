0fea1a913babc7bda615069bdc308d96f0eda386
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-02-28 17:39:44 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -12,7 +12,6 @@ def main():
     parser.add_argument("--warmup_steps", type=int, default=1000, help="warmup epochs")
     parser.add_argument("--lr_decay", type=float, default=0.9, help="apply lr decay or not")
     parser.add_argument("--data_path", type=str, default='none')
-    parser.add_argument("--ema_decay", type=float, default=0.99, help="Exponential Moving Average, default: 0.99")
     arg = parser.parse_args()
     print(arg)
     
@@ -27,10 +26,9 @@ def main():
     res_path.mkdir(exist_ok=True)
     print(f"result folder path: {res_path}")
     
-    dataset = pm.datasets.coco(
-        root=root,
-        dataType='train2017', 
-        annType='captions',
+    dataset = pm.load_dataset(
+        name='coco',
+        root=root, 
         transform=pm.create_transform(resize=320, crop_size=256, crop=True, is_train=True),
     )
         
@@ -41,21 +39,23 @@ def main():
     dataloader = pm.TxtImgDataloader(dataset, batch_size=arg.bs, shuffle=True, num_workers=0, pin_memory=False)
 
     trainer = pm.PaintMindTrainer(
-        model            =model, 
-        lr               =arg.lr, 
-        wd               =arg.wd, 
-        dataloader       =dataloader,
-        num_epochs       =arg.n_epoch,
-        lr_min           =0.5*arg.lr,
-        warmup_steps     =arg.wawarmup_steps,
-        warmup_lr_init   =0.1*arg.lr,
-        ema_decay        =arg.ema_decay,
-        max_grad_norm    =1.0, 
-        text_max_length  =77,
-        checkpoint_path  =res_path,
-        sample_interval  =1000,
-        vqf4_config_path ='./logs/vq_f4/config.yaml',
-        vqf4_pretrained_path=r'./logs/vq_f4/model.ckpt',
+        model                       = model,
+        lr                          = arg.lr, 
+        wd                          = arg.wd, 
+        dataloader                  = dataloader,
+        num_epochs                  = arg.n_epoch,
+        lr_min                      = 0.5*arg.lr,
+        warmup_steps                = arg.warmup_steps,
+        warmup_lr_init              = 0.1*arg.lr,
+        ema_decay                   = None,
+        max_grad_norm               = 1.0, 
+        text_max_length             = 256,
+        checkpoint_path             = res_path,
+        sample_interval             = 1000,
+        save_every_n_step           = 1000,
+        first_stage_config_path     = './models/first_stage_models/kl-f4/config.yaml',
+        first_stage_pretrained_path = './models/first_stage_models/kl-f4/model.ckpt',
+        gradient_accumulation_steps = 8,
     )
     
     trainer.train()
