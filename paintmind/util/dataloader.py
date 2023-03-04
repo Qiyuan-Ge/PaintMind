fbbe731b56f19dd53e1f5d5f6a4cca4e501e13a3
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-03-04 20:12:20 +0800

    Update dataloader.py

--- paintmind/util/dataloader.py
+++ paintmind/util/dataloader.py
@@ -1,11 +1,11 @@
 import torch
 from functools import partial
 from torch.utils.data import DataLoader
-from paintmind.text_encoder.t5 import tokenize, DEFAULT_T5_NAME
+from paintmind.text_encoder.clip import tokenize, DEFAULT_CLIP_NAME
 
     
 class collate_fn:
-    def __init__(self, text_model_name=DEFAULT_T5_NAME):
+    def __init__(self, text_model_name=DEFAULT_CLIP_NAME):
         self.text_model_name = text_model_name
         self.tokenize = partial(tokenize, name=text_model_name)
     
@@ -21,5 +21,5 @@ class collate_fn:
         return img_batch, input_ids, attn_mask
 
    
-def TxtImgDataloader(dataset, batch_size, shuffle=True, text_model_name=DEFAULT_T5_NAME, num_workers=0, pin_memory=False):
+def TxtImgDataloader(dataset, batch_size, shuffle=True, text_model_name=DEFAULT_CLIP_NAME, num_workers=0, pin_memory=False):
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn(text_model_name), pin_memory=pin_memory)
