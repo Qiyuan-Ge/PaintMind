3350b94481cd63803b9dbbe705f711af16976caf
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-03-08 13:16:42 +0800

    Update trainer.py

--- paintmind/util/trainer.py
+++ paintmind/util/trainer.py
@@ -26,34 +26,6 @@ def linear_masked_p_schedule(timesteps=75):
     p_max = 1.00
     
     return np.linspace(p_min, p_max, timesteps)
-
-def random_masking(x, mask_ratio):
-    """
-    Perform per-sample random masking by per-sample shuffling.
-    Per-sample shuffling is done by argsort random noise.
-    x: [N, L, D], sequence
-    """
-    
-    N, L, D = x.shape  # batch, length, dim
-    len_keep = int(L * (1 - mask_ratio))
-    
-    noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
-    
-    # sort noise for each sample
-    ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
-    ids_restore = torch.argsort(ids_shuffle, dim=1)
-
-    # keep the first subset
-    ids_keep = ids_shuffle[:, :len_keep]
-    x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
-
-    # generate the binary mask: 0 is keep, 1 is remove
-    mask = torch.ones([N, L], device=x.device)
-    mask[:, :len_keep] = 0
-    # unshuffle to get the binary mask
-    mask = torch.gather(mask, dim=1, index=ids_restore)
-
-    return x_masked, mask, ids_restore
     
 class Log:
     def __init__(self):
@@ -156,9 +128,8 @@ class PaintMindTrainer:
         save_image(transform_reverse(torch.cat([images, imgrec], dim=0)), image_path, nrow=n)
         
     def loss_func(self, x, label, mask):
-        one_minus_mask = 1 - mask
         loss = F.cross_entropy(x, label, reduction='none')
-        loss = (loss * mask).sum() / mask.sum() + (loss * one_minus_mask).sum() / (one_minus_mask.sum() + 1e-12)
+        loss = (loss * mask).sum() / mask.sum()
         
         return loss
     
@@ -180,16 +151,9 @@ class PaintMindTrainer:
                         with self.accelerator.autocast():
                             
                             z, indices = self.stage_one_model.encode(imgs)
-                            mask_ratio = np.random.choice(self.mask_ratio)
-                            b, c, h, w = z.shape
-                            z = rearrange(z, 'b c h w -> b (h w) c')
-                            z, mask, ids_restore = random_masking(z, mask_ratio)
-                            random_indices = torch.randint(0, 256, (b, ids_restore.shape[1]-z.shape[1]), device=z.device).long()
-                            replace_tokens = self.stage_one_model.get_embedding()(random_indices)
-                            z = torch.cat([z[:, :, :], replace_tokens], dim=1)
-                            z = torch.gather(z, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, z.shape[2])) #unshuffle
+                            # mask_ratio = np.random.choice(self.mask_ratio)
                             text = self.text_embbed(text)                      
-                            pred = self.stage_two_model(z, text)                     
+                            pred, mask = self.stage_two_model(z, text, mask_ratio=0.75)                     
                             loss = self.loss_func(pred.reshape(-1, pred.shape[-1]), indices.reshape(-1), mask.reshape(-1))
                             acc = self.compute_acc(pred.reshape(-1, pred.shape[-1]), indices.reshape(-1))
                     
@@ -221,6 +185,7 @@ class PaintMindTrainer:
                         self.save()
                         
                     if self.steps % self.sample_interval == 0:
+                        b, c, h, w = z.shape
                         self.sample(imgs, pred.detach(), n=4, h=h, w=w, c=c)
                        
             log.reset()
