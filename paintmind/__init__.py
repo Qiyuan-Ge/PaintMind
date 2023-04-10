0eb8606cc9ccdc8338ba07012a409ee0a8907b8f
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-04-10 15:06:26 +0800

    Delete paintmind directory

--- paintmind/__init__.py
+++
@@ -1,5 +0,0 @@
-from .version import __version__
-from .config import Config, create_model
-from .utils.transform import stage1_transform, stage2_transform
-from .utils.trainer import VQGANTrainer, PaintMindTrainer
-
