656e91a491b1fda88bae874dbe40ba365ef10613
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-04-11 17:53:34 +0800

    Update config.py

--- paintmind/config.py
+++ paintmind/config.py
@@ -2,7 +2,7 @@ import json
 from copy import deepcopy
 from .stage1 import VQModel
 from .pipeline import Pipeline
-
+from huggingface_hub import hf_hub_download
 
 class Config:
     def __init__(self, config=None):
@@ -78,14 +78,15 @@ vit_b_vqgan_config = {
 
 
 pipeline_v1_config = {
-    'vae'         :vit_s_vqgan_config,
-    'dim'         :768, 
-    'dim_context' :768, 
-    'dim_head'    :64,
-    'mlp_dim'     :3072,
-    'num_head'    :12, 
-    'depth'       :8, 
-    'dropout'     :0.1, 
+    'stage1_version' :'vit-s-vqgan',
+    'vae'            :vit_s_vqgan_config,
+    'dim'            :768, 
+    'dim_context'    :768, 
+    'dim_head'       :64,
+    'mlp_dim'        :3072,
+    'num_head'       :12, 
+    'depth'          :8, 
+    'dropout'        :0.1, 
 }
 
 
@@ -95,20 +96,32 @@ ver2cfg = {
     'pipeline-v1'  : pipeline_v1_config,
 }
 
-
-def create_model(arch='pipeline', version='pipeline-v1', pretrained=None, stage1_pretrained=None):
+def create_pipeline_for_train(version='pipeline-v1', stage1_pretrained=True, stage1_checkpoint_path=None):
     config = Config(config=ver2cfg[version])
+    if stage1_pretrained:
+        if stage1_checkpoint_path is None:
+            stage1_version = config.stage1_version
+            stage1_checkpoint_path = hf_hub_download("RootYuan/" + stage1_version, f"{stage1_version}.pt")
+        model = Pipeline(config, vae_pretrained=stage1_checkpoint_path)
+    else:
+        model = Pipeline(config)
+    
+    return model
+
+def create_model(arch='pipeline', version='pipeline-v1', pretrained=True, checkpoint_path=None):
+    config = Config(config=ver2cfg[version])
+    
     if arch == 'vqgan':
         model = VQModel(config)
     elif arch == 'pipeline':
-        model = Pipeline(config, vae_pretrained=stage1_pretrained)
+        model = Pipeline(config)
     else:
         raise ValueError(f"failed to load arch named {arch}")
         
-    if pretrained is not None:
-        model.from_pretrained(pretrained)
+    if pretrained:
+        if checkpoint_path is None:
+            checkpoint_path = hf_hub_download("RootYuan/" + version, f"{version}.pt")
+        model.from_pretrained(checkpoint_path)
         
     return model
         
-        
-    
