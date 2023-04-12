f252da1473cc94c1818279b612bbbfbc9287c9f6
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-04-12 17:16:16 +0800

    Update config.py

--- paintmind/config.py
+++ paintmind/config.py
@@ -57,7 +57,6 @@ vit_s_vqgan_config = {
     }, 
 }
 
-
 vit_b_vqgan_config = {
     'n_embed'     :8192,
     'embed_dim'   :32,
@@ -76,7 +75,6 @@ vit_b_vqgan_config = {
     }, 
 }
 
-
 pipeline_v1_config = {
     'stage1'         :'vit-s-vqgan',
     'dim'            :768, 
@@ -88,33 +86,9 @@ pipeline_v1_config = {
     'dropout'        :0.1, 
 }
 
-
 ver2cfg = {
     'vit-s-vqgan'  : vit_s_vqgan_config,
     'vit-b-vqgan'  : vit_b_vqgan_config,
     'pipeline-v1'  : pipeline_v1_config,
 }
-
-def create_pipeline_for_train(version='pipeline-v1', stage1_pretrained=True):
-    config = Config(config=ver2cfg[version])
-    model = Pipeline(config, stage1_pretrained=stage1_pretrained)
-    
-    return model
-
-def create_model(arch='pipeline', version='pipeline-v1', pretrained=True, checkpoint_path=None):
-    config = Config(config=ver2cfg[version])
-    
-    if arch == 'vqgan':
-        model = VQModel(config)
-    elif arch == 'pipeline':
-        model = Pipeline(config)
-    else:
-        raise ValueError(f"failed to load arch named {arch}")
-        
-    if pretrained:
-        if checkpoint_path is None:
-            checkpoint_path = hf_hub_download("RootYuan/" + version, f"{version}.pt")
-        model.from_pretrained(checkpoint_path)
-        
-    return model
         
