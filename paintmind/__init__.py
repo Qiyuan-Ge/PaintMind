bd73397836ae5a6fa125ad1df9c7761eeb7ffd91
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-04-12 09:21:16 +0800

    Update __init__.py

--- paintmind/__init__.py
+++ paintmind/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__
-from .config import Config, create_model
+from .config import Config, create_model, create_pipeline_for_train
 from .utils.transform import stage1_transform, stage2_transform
 from .utils.trainer import VQGANTrainer, PaintMindTrainer
 from .reconstruct import reconstruction
