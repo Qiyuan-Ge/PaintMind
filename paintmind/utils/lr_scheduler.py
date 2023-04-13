1c5d4ba908eb052da35cbf811b6e8dfe3484a2d1
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-04-13 15:41:27 +0800

    Update lr_scheduler.py

--- paintmind/utils/lr_scheduler.py
+++ paintmind/utils/lr_scheduler.py
@@ -1,7 +1,8 @@
 from timm.scheduler.cosine_lr import CosineLRScheduler
 
-def build_scheduler(optimizer, n_epoch, n_iter_per_epoch, lr_min, warmup_steps, warmup_lr_init):
-    num_steps = n_epoch * n_iter_per_epoch
-    scheduler = CosineLRScheduler(optimizer, t_initial=num_steps, lr_min=lr_min, warmup_t=warmup_steps, warmup_lr_init=warmup_lr_init, cycle_limit=1, t_in_epochs=False)
+def build_scheduler(optimizer, n_epoch, n_iter_per_epoch, lr_min, warmup_steps, warmup_lr_init, decay_steps=None):
+    if decay_steps is None:
+        decay_steps = n_epoch * n_iter_per_epoch
+    scheduler = CosineLRScheduler(optimizer, t_initial=decay_steps, lr_min=lr_min, warmup_t=warmup_steps, warmup_lr_init=warmup_lr_init, cycle_limit=1, t_in_epochs=False)
     
     return scheduler
