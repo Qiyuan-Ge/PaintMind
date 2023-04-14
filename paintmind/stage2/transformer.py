ea28f83e06dcccb4a2677a0c3bbcdbc586324fb8
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-04-14 23:57:48 +0800

    Update transformer.py

--- paintmind/stage2/transformer.py
+++ paintmind/stage2/transformer.py
@@ -23,42 +23,50 @@ def default(val, d):
         return val
     return d() if isfunction(d) else d
 
-
+    
 class CrossAttention(nn.Module):
-    def __init__(self, query_dim, context_dim=None, heads=8, dim_head=64, dropout=0.0):
+    def __init__(self, query_dim, context_dim=None, heads=8, dim_head=64, dropout=0.):
         super().__init__()
         inner_dim = dim_head * heads
         context_dim = default(context_dim, query_dim)
-        self.num_head = heads
+
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+
         self.to_q = nn.Linear(query_dim, inner_dim, bias=False)
         self.to_k = nn.Linear(context_dim, inner_dim, bias=False)
         self.to_v = nn.Linear(context_dim, inner_dim, bias=False)
-        self.attn_drop = nn.Dropout(p=dropout)
-        self.to_o = nn.Linear(inner_dim, query_dim)
-        
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, query_dim),
+            nn.Dropout(dropout)
+        )
+
     def forward(self, x, context=None, mask=None):
-        context = default(context, x)
-        
+        h = self.heads
+
         q = self.to_q(x)
+        context = default(context, x)
         k = self.to_k(context)
         v = self.to_v(context)
-        
-        q, k, v = [rearrange(x, 'b n (h d) -> b h n d', h=self.num_head) for x in (q, k, v)]
-        attn = torch.einsum('b h i d, b h j d -> b h i j', q, k) / math.sqrt(q.shape[-1])
-        
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
+
+        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
+    
         if exists(mask):
-            mask = rearrange(mask, 'b j -> b 1 1 j')
-            max_neg_value = -torch.finfo(attn.dtype).max
-            attn = attn.masked_fill(~mask, max_neg_value)
-            
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-        x = torch.einsum('b h i j, b h j d -> b h i d', attn, v)
-        x = rearrange(x, 'b h n d -> b n (h d)')
-        x = self.to_o(x)
+            mask = rearrange(mask, 'b ... -> b (...)')
+            max_neg_value = -torch.finfo(sim.dtype).max
+            mask = repeat(mask, 'b j -> (b h) () j', h=h)
+            sim.masked_fill_(~mask, max_neg_value)
 
-        return x
+        # attention, what we cannot get enough of
+        sim = sim.softmax(dim=-1)
 
+        out = einsum('b i j, b j d -> b i d', sim, v)
+        out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
+        return self.to_out(out)    
+    
 
 class MemoryEfficientCrossAttention(nn.Module):
     # https://github.com/MatthieuTPHR/diffusers/blob/d80b531ff8060ec1ea982b65a1b8df70f73aa67c/src/diffusers/models/attention.py#L223
@@ -76,7 +84,10 @@ class MemoryEfficientCrossAttention(nn.Module):
         self.to_k = nn.Linear(context_dim, inner_dim, bias=False)
         self.to_v = nn.Linear(context_dim, inner_dim, bias=False)
 
-        self.to_out = nn.Sequential(nn.Linear(inner_dim, query_dim), nn.Dropout(dropout))
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, query_dim), 
+            nn.Dropout(dropout)
+        )
         self.attention_op: Optional[Any] = None
 
     def forward(self, x, context=None, mask=None):
